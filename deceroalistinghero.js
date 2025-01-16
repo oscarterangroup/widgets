1d8fa4a3ce60d4824c463656eea99e9c208ed8fa
diegopaeze97 <diegopaeze@gmail.com>
2025-01-16 14:04:39 -0400

    modified:   deceroalistinghero.js modified:   test.html

--- deceroalistinghero.js
+++ deceroalistinghero.js
@@ -2,20 +2,26 @@
     const style = document.createElement('style');
     style.innerHTML = `
         :root {
-            --color-primario: #121275;  /* azul oscuro */
-            --color-secundario: #041723;  /* Azul dark*/
-            --color-terciario: #FFFFFF;   /* Blanco */
-            --color-gris: #888A8E;       /* Gris claro */
-            --transparent-bg: rgba(0,0,0,0.10);
+            --color-primario: #121275;
+            /* azul oscuro */
+            --color-secundario: #041723;
+            /* Azul dark*/
+            --color-terciario: #FFFFFF;
+            /* Blanco */
+            --color-gris: #888A8E;
+            /* Gris claro */
+            --transparent-bg: rgba(0, 0, 0, 0.10);
             --site-title-font: "Averta-Semibold";
             --site-paragraph-font: "AvertaStd-Light";
             --site-titlesec-font: "AvertaDemo-ExtraBoldItalic";
             --border-btn: 50px;
             --border-cont: 20px;
         }
+    
         body {
             margin: 0;
         }
+    
         .my-widget-content {
             margin: 0;
             display: flex;
@@ -26,6 +32,7 @@
             background-repeat: no-repeat;
             width: 100%;
         }
+    
         .my-widget-content-text {
             flex: 1;
             display: flex;
@@ -36,31 +43,37 @@
             font-family: var(--site-title-font);
             padding-left: 3rem;
         }
-        .my-widget-content-text img{
+    
+        .my-widget-content-text img {
             width: 27%;
         }
-        .my-widget-content-text .my-widget-content-text-inner h2{
+    
+        .my-widget-content-text .my-widget-content-text-inner h2 {
             font-size: 42px;
             margin: 0%;
             text-transform: uppercase;
             color: var(--color-terciario);
         }
-        .my-widget-content-text .my-widget-content-text-inner h1{
+    
+        .my-widget-content-text .my-widget-content-text-inner h1 {
             font-size: 104px;
             margin: 0%;
             text-transform: uppercase;
             font-family: var(--site-titlesec-font);
             color: var(--color-terciario);
         }
-        .my-widget-content-text .my-widget-content-text-inner h3{
+    
+        .my-widget-content-text .my-widget-content-text-inner h3 {
             font-size: 36px;
             margin: 0%;
             color: var(--color-terciario);
         }
-        .my-widget-content-text .my-widget-content-text-inner p{
+    
+        .my-widget-content-text .my-widget-content-text-inner p {
             max-width: 80%;
             color: var(--color-terciario);
         }
+    
         .otg-button-111 {
             height: 60px;
             background-color: var(--color-secundario);
@@ -73,7 +86,7 @@
             text-transform: uppercase;
             transition: background-color 1.0s ease;
         }
-
+    
         .otg-button-111:hover {
             background-color: var(--color-terciario);
             min-width: 250px;
@@ -82,6 +95,7 @@
             border-radius: var(--border-btn);
             cursor: pointer;
         }
+    
         .my-widget-content-pic {
             flex: 1;
             display: flex;
@@ -89,34 +103,93 @@
             align-items: center;
             justify-content: flex-end;
         }
-        .my-widget-content-pic img{
+    
+        .my-widget-content-pic img {
             width: 100%;
         }
+    
+        p {
+            margin-bottom: 0;
+        }
+    
+        .nav-1111 {
+            display: flex;
+            height: 100px;
+            justify-content: space-between;
+            align-items: center;
+            margin: 0px 40px 0px 40px;
+            padding: 0 40px 0 0;
+            background-color: transparent;
+            z-index: 12;
+        }
+    
+        .nav-1111 a {
+            font-family: var(--site-title-font);
+            text-decoration: none;
+            color: var(--color-terciario) !important;
+            font-size: 1.1rem !important;
+        }
+    
+        .nav-1111-left {
+            width: 18%;
+        }
+    
+        .nav-1111-left img {
+            width: 100%;
+        }
+    
+        .nav-1111-right {
+            justify-content: center;
+            align-items: center;
+        }
+    
+        .nav-1111-right a {
+            padding: 0 20px;
+        }
+        .mobile {
+            display: none;
+        }
 
+        @media screen and (max-width: 479px) {
+            .mobile {
+                display: block;
+                width: 100%;
+            }
+
+            .mobile img{
+                display: block;
+                width: 100%;
+            }
+            .my-widget-content-pic, .my-widget-content-text {
+                display: none;
+            }
+        }
+    
         /* AvertaStd-Light */
         @font-face {
-        font-family: "AvertaStd-Light";
-        src: url('https://oscarterangroup.github.io/widgets/imgs/Averta-light.woff.ttf') format('truetype');
-        font-weight: 400;
-        font-style: normal;
-        font-display: swap;
+            font-family: "AvertaStd-Light";
+            src: url('https://oscarterangroup.github.io/widgets/imgs/Averta-light.woff.ttf') format('truetype');
+            font-weight: 400;
+            font-style: normal;
+            font-display: swap;
         }
+    
         /* Averta-Semibold */
         @font-face {
-        font-family: "Averta-Semibold";
-        src: url('https://oscarterangroup.github.io/widgets/imgs/Averta-Semibold.ttf') format('truetype');
-        font-weight: 400;
-        font-style: normal;
-        font-display: swap;
+            font-family: "Averta-Semibold";
+            src: url('https://oscarterangroup.github.io/widgets/imgs/Averta-Semibold.ttf') format('truetype');
+            font-weight: 400;
+            font-style: normal;
+            font-display: swap;
         }
-
+    
         /* Averta-Semibold */
         @font-face {
-        font-family: "AvertaDemo-ExtraBoldItalic";
-        src: url('https://oscarterangroup.github.io/widgets/imgs/AvertaDemo-ExtraBoldItalic.otf') format('truetype');
-        font-weight: 400;
-        font-style: normal;
-        font-display: swap;
+            font-family: "AvertaDemo-ExtraBoldItalic";
+            src: url('https://oscarterangroup.github.io/widgets/imgs/AvertaDemo-ExtraBoldItalic.otf') format('truetype');
+            font-weight: 400;
+            font-style: normal;
+            font-display: swap;
         }
     `;
     document.head.appendChild(style);
@@ -133,10 +206,13 @@
                     <h3>El Curso que Transformará <br> Tu Carrera Inmobiliaria</h3>
                     <p>INCLUYE UN EBOOK CON COMANDOS DE INTELIGENCIA ARTIFICIAL DISEÑADOS PARA IMPULSAR TUS VENTAS HOY</p>
                 </div>
-                <button class="otg-button-111">QUIERO ESTE CURSO</button>
+                <a href="#section-1734730516064"><button class="otg-button-111">QUIERO ESTE CURSO</button></a>
             </div>
             <div class="my-widget-content-pic">
                 <img src="https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/file-uploads/themes/2159589961/settings_images/a4d8df3-841-c11-36ae-88be1bcf178c_c54fa26a-a505-4acb-af52-e6295643ef61.png" alt="Oscar Teran picture">
+            </div>
+            <div class="mobile">
+                <img src="https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/file-uploads/themes/2159589961/settings_images/e4a164-506e-6e0-d658-5d1bc86c43a_THMBNAILCURSO.jpg" alt="banner de cero a listing">
             </div>
         </div>
         `;
